658605ac5e74a03008034d1534787975fe65dddb
Ramesh138 <ponneriramesh158@gmail.com>
2020-06-01 12:11:23 +0530

    01/05/2020

--- src/com/seleniumprograms/testng/Test1.java
+++ src/com/seleniumprograms/testng/Test1.java
@@ -2,15 +2,23 @@ package com.seleniumprograms.testng;
 
 
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Test1 {
 
+	
+	
 	@Test
 	public void test1(){
 		System.out.println("Test 1");
 		Assert.assertTrue(true);
+		WebDriver driver = null;
+		
+		String actualvalue=driver.findElement(By.xpath("")).getText();
+		Assert.assertEquals("Method", actualvalue);
 		
 	}
 }
